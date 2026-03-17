fe35fd2010bde53e53be6a5120dc527570060c13
giustoantonadev <giustoantonadev@gmail.com>
2026-03-17 14:38:39 +0100

    Header, some fixes on card, little bit of bootstrap/css

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -36,19 +36,19 @@ export default function AppCard() {
 
                 {
                     actors.map(actor => (
-                        <div className="col">
-                            <div className="card" style={{ width: "18rem" }}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{actor.name}</h5>
+                        <div className="col col-6 col-md-4 col-lg-4 col-xl-3 d-flex text-center pb-4" key={actor.id}>
+                            <div className="card bg-dark w-100 h-100">
+                                <div className="card-body bg-dark">
+                                    <h5 className="card-title text-white">{actor.name}</h5>
                                     <img src={actor.image} className="card-img-top" alt="photo" />
-                                    <p className="card-text">{actor.birth_year} {actor.nationality}</p>
+                                    <p className="card-text text-white"><em>{actor.birth_year}  {actor.nationality}</em></p>
                                 </div>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">{actor.biography}</li>
-                                    <li className="list-group-item text-center">Known for: <br /> {actor.known_for}</li>
-                                    <li className="list-group-item">{actor.awards}</li>
+                                <ul className="list-group list-group-flush mt-auto">
+                                    <li className="list-group-item bg-dark text-white border-0">{actor.biography}</li>
+                                    <li className="list-group-item text-center bg-dark border-0 text-danger"><strong><em>Known for:</em></strong> <br />{actor.known_for ? actor.known_for.join(', ') : 'None'} </li>
+                                    <li className="list-group-item bg-dark text-warning"><strong><em>Awards:</em></strong> <br />{actor.awards ? actor.awards.join(', ') : 'None'}</li>
+
                                 </ul>
-                                
                             </div>
                         </div>
                     ))
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,9 +4,20 @@ export default function AppHeader() {
 
     return (
 
+        <>
+            <nav className="bg-dark">
+                <div class="container-fluid text-center">
+                    <p className="text-center text-white p-2">Wonderful Cast</p>
+                </div>
+            </nav>
 
-        <header>test</header>
+            <header className="text-center">
+                <h1>Actors</h1>
+                <p>List of actors fetched from an API</p>
+            </header>
 
+
+        </>
 
     )
 }
