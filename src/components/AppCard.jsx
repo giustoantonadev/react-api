4d14ebd8f3bc66f66e6578508fa0e161568871ba
giustoantonadev <giustoantonadev@gmail.com>
2026-03-17 12:34:46 +0100

    actor card(not finished)

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -31,11 +31,34 @@ export default function AppCard() {
     useEffect(getActresses, [])
     return (
 
-        <main>
+        <div className="container">
+            <div className="row">
 
-        </main>
+                {
+                    actors.map(actor => (
+                        <div className="col">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{actor.name}</h5>
+                                    <img src={actor.image} className="card-img-top" alt="photo" />
+                                    <p className="card-text">{actor.birth_year} {actor.nationality}</p>
+                                </div>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">{actor.biography}</li>
+                                    <li className="list-group-item text-center">Known for: <br /> {actor.known_for}</li>
+                                    <li className="list-group-item">{actor.awards}</li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+                    ))
+                }
 
 
+
+            </div>
+        </div>
 
     )
 }
+
